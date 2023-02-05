3c2f0782575b9cff684cb7613f336a7016aa6cb8
Kemal Şahin <kemals@rocketmail.com>
2023-02-05 13:20:43 +0300

    LeetcodeSol

---
+++ src/main/java/mySqrt.java
@@ -0,0 +1,13 @@
+public class mySqrt {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(8));
+    }
+    public static int mySqrt(int x) {
+        if(x==0) return 0;
+        int i=1;
+        for(;i<=x/i;i++) //using division instead of multiplication to avoid integer overflow
+            if(x/i==i)  return i;
+        return i-1;
+    }
+}
+
